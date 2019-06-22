9f1bd8bf5a061cfe7344caed25987a2d2abfe7af
uniorunr <gagael23@live.com>
2019-06-22 16:50:12 +0300

    feature: dynamically change map layer depending on language uri in url

--- scripts/after-map-load.js
+++ scripts/after-map-load.js
@@ -14,3 +14,12 @@ label.addEventListener('touchstart', labelClick);
 
 layerIcon.appendChild(label);
 
+const url = window.location.href;
+
+if (url.indexOf('/en/') !== -1) {
+  const labels = [].slice.call(document.querySelectorAll('input[name="leaflet-base-layers"]'));
+  const osmDe = labels.find(function (label) {
+    return label.parentElement.lastElementChild.textContent.indexOf('OSM DE') !== -1;
+  });
+  osmDe.click();
+}
